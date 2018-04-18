e14960d956b68b9c6a3becc971a42578eb9bbcae
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-04-18 23:09:06 +0200

    implement actor pull method

--- test/domain/actor/actor.test.js
+++ test/domain/actor/actor.test.js
@@ -2,16 +2,30 @@ import Actor from "../../../lib/domain/actor/actor";
 
 let createActor = () => {
     return new (class extends Actor {
+        constructor() {
+            super();
 
+            this.onReceive = jest.fn();
+        }
     })();
 };
 
 describe("Actor", () => {
-    test("mailbox", () => {
+    test("should put the received message at the end of the mailbox", () => {
         let actor = createActor();
         actor.receiveMessage(1);
 
         let [ message ] = actor.mailbox;
         expect(message).toBe(1);
+    });
+
+    test("should pull a message and call onReceive with that message", () => {
+        let actor = createActor();
+        actor.receiveMessage(1);
+
+        actor.pull();
+
+        expect(actor.mailbox).toEqual([]);
+        expect(actor.onReceive.mock.calls[0][0]).toBe(1);
     });
 });
