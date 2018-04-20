b801212a34f3946e82cd2179c02cfa8fa563a0f1
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-04-20 19:38:51 +0200

    Retry strategy with maximum retries

--- test/domain/actor/supervisor/retry.test.js
+++ test/domain/actor/supervisor/retry.test.js
@@ -2,12 +2,24 @@ import Actor from "../../../../lib/domain/actor/actor";
 import TimeMachine from "../../../../lib/domain/actor/time-machine";
 import retry from "../../../../lib/domain/actor/supervisor/retry";
 
+const system = {maxRetries: 2};
 describe("Retry strategy", () => {
     test("should retry the latest message with the old state", () => {
         let actor = new Actor([{ message: 1 }, { message: 2 }], new TimeMachine([{state: 1}, {state: 2}]));
-        let result = retry(undefined, actor);
+
+        let result = retry(system, actor);
 
         expect(result.state).toEqual(1);
         expect(result.mailbox).toEqual([{ message: 1, retries: 1 }, { message: 2 }]);
+    });
+
+    test("should retry at most the maximum times specified in the system and drop the message", () => {
+        let actor = new Actor([{ message: 1 }, { message: 2 }], new TimeMachine([{state: 1}, {state: 2}]));
+        retry(system, actor);
+        retry(system, actor);
+        let result = retry(system, actor);
+
+        expect(result.state).toEqual(1);
+        expect(result.mailbox).toEqual([{ message: 2 }]);
     });
 });
