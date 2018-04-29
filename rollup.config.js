ff27cc11e3f488f35fafddfd0622321d74a132db
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-04-29 23:42:25 +0200

    Build with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,18 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
     input: "lib/index.js",
-
+    plugins: [babel({
+        "plugins": [
+            [
+                "transform-object-rest-spread",
+                {
+                    "useBuiltIns": false
+                }
+            ]
+        ]
+        }
+    )],
     output: [{
         name: "wind",
         format: "cjs",
