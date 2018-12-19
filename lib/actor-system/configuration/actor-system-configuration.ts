b6c2b5fb5f7f7ab0b4070ba098b3ed070310d8cc
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-12-19 16:05:31 +0100

    Unused imports

--- lib/actor-system/configuration/actor-system-configuration.ts
+++ lib/actor-system/configuration/actor-system-configuration.ts
@@ -1,7 +1,3 @@
-import Mailbox from '../../mailbox/mailbox'
-import ActorMessage from '../actor-message'
-import IMaterializer from '../materializer/materializer'
-import IResolver from '../resolver/resolver'
 import ActorSystemConfigurationBuilder from './actor-system-configuration-builder'
 
 type Readonly<T> = { readonly [P in keyof T]: T[P] }
