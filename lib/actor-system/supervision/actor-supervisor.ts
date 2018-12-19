404645e9ca6b6fc5496913e06d963f0dff6b3330
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-12-19 16:43:55 +0100

    Basic supervision refactor

--- lib/actor-system/supervision/actor-supervisor.ts
+++ lib/actor-system/supervision/actor-supervisor.ts
@@ -1,5 +1,7 @@
 import { Actor } from '../..'
 
+export type SupervisionResponse = 'drop-message' | 'retry-message' | 'kill-actor'
+
 export default interface IActorSupervisor {
-  supervise(actor: Actor, exception: any): void
+  supervise(actor: Actor, exception: any): SupervisionResponse
 }
--- lib/actor-system/supervision/noop-supervisor.ts
+++ lib/actor-system/supervision/noop-supervisor.ts
@@ -1,8 +1,9 @@
 import { Actor } from '../..'
 import IActorSupervisor from './actor-supervisor'
+import { SupervisionResponse } from './actor-supervisor'
 
 export default class NoopActorSupervisor implements IActorSupervisor {
-  public supervise(actor: Actor, exception: any): void {
-    return
+  public supervise(actor: Actor, exception: any): SupervisionResponse {
+    return 'drop-message'
   }
 }
