f218fc2dc170ca1b3458d266f1283b105a3e9c36
kanekotic <alvarojosepl@gmail.com>
2018-12-24 14:40:21 -0600

    added direct reexported targets

--- lib/index.ts
+++ lib/index.ts
@@ -4,21 +4,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import Actor from './actor-system/actor'
-import ActorSystem from './actor-system/actor-system'
-import ActorSystemConfigurationBuilder from './actor-system/configuration/actor-system-configuration-builder'
-import IMaterializer from './actor-system/materializer/materializer'
-import IResolver from './actor-system/resolver/resolver'
-import IActorSupervisor, { SupervisionStrategies } from './actor-system/supervision/actor-supervisor'
-import Topic from './pubsub/topic'
 
+export { default as Actor } from './actor-system/actor'
+export { default as ActorSystem } from './actor-system/actor-system'
 export {
-  Actor,
-  ActorSystem,
-  ActorSystemConfigurationBuilder,
-  Topic,
-  IActorSupervisor,
-  SupervisionStrategies,
-  IResolver,
-  IMaterializer,
-}
+  default as ActorSystemConfigurationBuilder,
+} from './actor-system/configuration/actor-system-configuration-builder'
+export { default as IMaterializer } from './actor-system/materializer/materializer'
+export { default as IResolver } from './actor-system/resolver/resolver'
+export { default as IActorSupervisor, SupervisionStrategies } from './actor-system/supervision/actor-supervisor'
+export { default as Topic } from './pubsub/topic'
